6c7d066c3b2ba537161fd41b0e035ab55e2d735b
Gorrela Swami Siva Rama Raju <133990765+sivaramaraju2124@users.noreply.github.com>
2025-12-02 12:46:17 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use("/api/attendance", require("./routes/attendanceRoutes"));
 // Serve React build from backend/build
 app.use(express.static(path.join(__dirname, "build")));
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
